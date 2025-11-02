479fdf701b7e5be276771cd10d9e29bdaa1df147
Radwan Alhourani <32372529+rhourani@users.noreply.github.com>
2025-11-02 19:51:06 +0200

    Adding project

---
+++ README.md
@@ -0,0 +1,7 @@
+# Simple Node Project
+
+Install and run:
+1. Open terminal in this folder.
+2. npm install
+3. npm start
+4. Visit http://localhost:3000
---
+++ index.js
@@ -0,0 +1,17 @@
+const http = require('http');
+const express = require('express');
+const port = process.env.PORT || 3000;
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' });
+  res.end('Hello World\n');
+});
+
+const app = express();
+app.get('/', (req, res) => {
+  res.type('text').send('Hello World\n');
+});
+
+server.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}/`);
+});
